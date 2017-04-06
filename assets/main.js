4e0f4a502ef72a218eec1adae0e00491f09ce829
Itzel <itzeljimenez.do@gmail.com>
2017-04-06 14:15:31 +0000

    reconocimiento de reclado con switch

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,69 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
 
 var snake = document.getElementById('snake');
-var seccion = document.getElementById('seccion');
 var xi=0;
 var yi=0;
 var xf=400;
 var yf=400;
+var constante = 100;
 
 function moverDerecha(){
 	if(xi<xf){
-		snake.style.marginLeft = ((xi+=100) +"px").toString();
+		snake.style.marginLeft = ((xi+=constante) +"px").toString();
 	} else{
 		snake.style.marginLeft = "400px"
-		alert("perdiste")
+		perdiste()
 	}
 }
 function moverIzq(){
 	if(xf>xi){
-		snake.style.marginLeft = ((xi-=100) +"px").toString();
+		snake.style.marginLeft = ((xi-=constante) +"px").toString();
 	} else{
-		snake.style.marginLeft = "0px"
-		alert("perdiste")
+		perdiste()
 	}
 }
 function moverAbajo(){
 	if(yi<yf){
-		snake.style.marginTop = ((yi+=100) +"px").toString();
+		snake.style.marginTop = ((yi+=constante) +"px").toString();
 	} else{
-		snake.style.marginTop = "400px"
-		alert("perdiste")
+		perdiste()
 	}
 }
 function moverArriba(){
 	if(yf>yi){
-		snake.style.marginTop = ((yi-=100) +"px").toString();
+		snake.style.marginTop = ((yi-=constante) +"px").toString();
 	} else{
-		snake.style.marginTop = "0px"
-		alert("perdiste")
+		perdiste()
 	}
 }
+function perdiste(){
+	alert("perdiste")
+}
+function teclaPulsada(evento){
+	console.log('evento')
+		switch(evento.keyCode){
+    case teclas.UP:
+    	moverArriba()
+    break;
+    case teclas.DOWN:
+    	moverAbajo()
+    break;
+    case teclas.LEFT:
+    	moverIzq()
+    break;
+    case teclas.RIGHT:
+    	moverDerecha()
+    break;
+  }
+		
+}
+document.addEventListener("keydown", teclaPulsada)
+
 
 //EVENTOS
-document.addEventListener("click", moverAbajo)
-document.addEventListener("keypress", moverArriba)
+
