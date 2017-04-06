351ad734ef3f880f2fd4669936989f74d7f31bcb
Itzel <itzeljimenez.do@gmail.com>
2017-04-06 11:33:22 +0000

    atributos de estilo, contenedor y serpiente

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
 alert("washu banda!")
+var snake = document.getElementById('snake');
+var seccion = document.getElementById('seccion');
+
+
+function mover(){
+	
+}
+document.addEventListener("click", mover)
