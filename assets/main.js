8044f4b64baa1e5b3dda177fd7f685a828ffe45e
Itzel <itzeljimenez.do@gmail.com>
2017-04-06 12:30:39 +0000

    manipulación del div con eventos básicos

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,19 @@
-alert("washu banda!")
+
 var snake = document.getElementById('snake');
 var seccion = document.getElementById('seccion');
+var x=0;
+var y=0;
+var i=0;
 
 
-function mover(){
-	
+function moverDerecha(){
+	snake.style.marginLeft = "100px"	
 }
-document.addEventListener("click", mover)
+function moverAbajo(){
+	snake.style.marginTop = "100px"
+}
+
+
+//EVENTOS
+document.addEventListener("click", moverDerecha)
+document.addEventListener("keypress", moverAbajo)
